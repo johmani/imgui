d2d0c3b18a3b5eac8b254477acc8b632e7279800
Mohamd Johmani <mohamdaljohmani@gmail.com>
2025-06-16 14:40:11 +0300

    Use spaces instead of taps

--- premake5.lua
+++ premake5.lua
@@ -1,41 +1,41 @@
 project "imgui"
-	kind "SharedLib"
-	language "C++"
-	cppdialect "C++latest"
-	staticruntime "Off"
+    kind "SharedLib"
+    language "C++"
+    cppdialect "C++latest"
+    staticruntime "Off"
 
-	targetdir (binOutputDir)
+    targetdir (binOutputDir)
     objdir (IntermediatesOutputDir)
 
-	files
-	{
-		"*.h",
-		"*.cpp",
-		"*.cppm",
-		"*.lua",
-	}
+    files
+    {
+        "*.h",
+        "*.cpp",
+        "*.cppm",
+        "*.lua",
+    }
 
     removefiles{
 
         "imgui_demo.cpp",
     }
 
-	filter "system:windows"
-		systemversion "latest"
+    filter "system:windows"
+        systemversion "latest"
 
-	filter "system:linux"
-		pic "On"
-		systemversion "latest"
+    filter "system:linux"
+        pic "On"
+        systemversion "latest"
 
-	filter "configurations:Debug"
-		runtime "Debug"
-		symbols "On"
+    filter "configurations:Debug"
+        runtime "Debug"
+        symbols "On"
 
-	filter "configurations:Release"
-		runtime "Release"
-		optimize "On"
+    filter "configurations:Release"
+        runtime "Release"
+        optimize "On"
 
-	filter "configurations:Dist"
-		runtime "Release"
-		optimize "Speed"
-		symbols "Off"
+    filter "configurations:Dist"
+        runtime "Release"
+        optimize "Speed"
+        symbols "Off"
